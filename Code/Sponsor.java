25e75eff52bcc54801342c063d7a7d79d0f9f686
ghousse <158365722+Goussee@users.noreply.github.com>
2024-09-27 15:50:55 -0500

    Create Sponsor.java

---
+++ Code/Sponsor.java
@@ -0,0 +1,19 @@
+public class Sponsor{
+    private final int[] cost;
+    private final int value;
+    public Sponsor(){
+        //order is r-g-b-w-o
+        cost = new int[]{0, 0, 0, 0, 0};
+        value = 0;
+    }
+    public Sponsor(int[] cost, int value, String type){
+        this.cost = cost;
+        this.value = value;
+    }
+    public int[] getCost(){
+        return cost;
+    }
+    public int getValue(){
+        return value;
+    }
+}
