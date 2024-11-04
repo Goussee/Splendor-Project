673675802c9b6d8e98fb4490edae7f6f56869439
suvina346 <suvinavijayendran27@gmail.com>
2024-11-04 08:09:41 -0600

    Card.java

--- Code/Card.java
+++ Code/Card.java
@@ -1,27 +1,30 @@
+import java.util.HashMap;
 
 public class Card{
-    private final int[] cost;
+    private final HashMap<String, Integer> cost;
     private final int value;
     private final int points;
     private final int tier;
+    private final String type;
     private final String color;
     public Card(){
         //order is r,g,b,w,o
-        //secret
-        cost = new int[]{0, 0, 0, 0, 0};
+        cost = new HashMap<String, Integer>();
         tier = 1;
         value = 1;
         points = 0;
+		type = "";
         color = "uninstantiated";
     }
-    public Card(int[] cost, int tier, int value, int points, String color){
+    public Card( HashMap<String, Integer> cost, int tier, int value, int points, String type,String color){
         this.cost = cost;
         this.tier = tier;
         this.value = value;
         this.points = points;
+		this.type = type;
         this.color = color;
     }
-    public int[] getCost(){
+    public HashMap<String, Integer> getCost(){
         return cost;
     }
     public int getTier() {
