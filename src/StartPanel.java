aed512b83bfc89f15f7b8a41312f2abe630a26ab
Goussee <gavinowells@gmail.com>
2024-11-18 08:03:53 -0600

    Working on StartPanel

--- src/StartPanel.java
+++ src/StartPanel.java
@@ -7,14 +7,15 @@ public class StartPanel extends ParentPanel implements MouseListener{
     private JFrame frame;
     private boolean showRules;
     private boolean showNumPlayers;
-    private Image bg;
+    private Image startImg, rulesImg, chooseImage;
     public StartPanel(){
         System.out.println();
         showRules = true;
         showNumPlayers = false;
-        bg = null;
+        startImg = null;
         try{
-            bg = ImageIO.read(getClass().getResource("Startscreen_Background.PNG"));
+            startImg = ImageIO.read(getClass().getResource("StartScreenBackground.PNG"));
+            chooseImg = ImageIO.read(getClass().getResource("StartScreenPlayers.PNG"));
         }catch(Exception e){
             System.out.println(e);
         }
@@ -22,17 +23,28 @@ public class StartPanel extends ParentPanel implements MouseListener{
     }
     public void paint(Graphics g){
         super.paint(g);
-        g.drawImage(bg, 0, 0, getWidth(), getHeight(), null);
+        g.setColor(Color.RED);
+
         if(!(showRules || showNumPlayers)){
-            //show "play" and "rules"
+            g.drawImage(startImg, 0, 0, getWidth(), getHeight(), null);
+            g.drawRect(getWidth()/2-110, getHeight()/2+45, 220, 90);
+            g.drawRect(getWidth()/2-135, getHeight()/2+180, 270, 90);
+        } else if(showRules){
+            g.drawImage(rulesImg, 0, 0, getWidth(), getHeight(), null);
         }
-        if(showRules){
-            
-        }
+
     }
     
     public void mouseClicked(MouseEvent e) {
-        showRules = !showRules;
+        int x = e.getX(), y = e.getY();
+        if(!(showRules||showNumPlayers)){
+            showNumPlayers = x>getWidth()-110&&x<getWidth()+110&&y>getHeight()+45&&y<getHeight()+135;
+            showRules = x>getWidth()-135&&x<getWidth()+270&&y>getHeight()+180&&y<getHeight()+270;
+        } else if(showRules){
+            showRules = false;
+        } else if(showNumPlayers){
+
+        }
         repaint();
     }
     public void mousePressed(MouseEvent e) {
