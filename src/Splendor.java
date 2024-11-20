47db7c331022b40dd1f11745ed7731147342f0a8
Goussee <gavinowells@gmail.com>
2024-11-20 07:28:50 -0600

    Changes Player and Splendor classes

--- src/Splendor.java
+++ src/Splendor.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -19,6 +20,9 @@ public class Splendor{
         deck1 = new Stack<>();
         deck2 = new Stack<>();
         deck3 = new Stack<>();
+        seen1 = new Card[4];
+        seen2 = new Card[4];
+        seen3 = new Card[4];
         int numPlayers = (n < 2) ? 2 : (n > 4) ? 4 : n;
         int numTokens = (numPlayers == 4) ? 7 : (numPlayers == 3) ? 5 : 4;
         winner = null;
@@ -47,6 +51,11 @@ public class Splendor{
                 deck2.add(c);
             else if(c.getLevel() == 3)
                 deck3.add(c);
+        }
+        for(int i = 0; i < 4; i++){
+            seen1[i] = deck1.pop();
+            seen2[i] = deck2.pop();
+            seen3[i] = deck3.pop();
         }
 
         scan.close();
@@ -92,7 +101,7 @@ public class Splendor{
         gems.put(gem2, gems.get(gem2) - 1);
         gems.put(gem3, gems.get(gem3) - 1);
     }//end of draw3
-
+    
     public void endTurn(){
         while(players[currentPlayer].getGemSum() > 10){
             gameState = "removeTokens";
@@ -103,28 +112,67 @@ public class Splendor{
                 endGame();
                 return;
             }
+        } else {
+            isEnding = players[currentPlayer].getPoints() >= 15;
         }
-        isEnding = players[currentPlayer].getPoints() >= 15;
         currentPlayer = currentPlayer == players.length - 1 ? 0 : currentPlayer + 1;
     }//end of endTurn
 
     public boolean isEnding(){
-        return false;
+        return isEnding;
     }//end of isEnding
 
     public void endGame(){
-
+        winner = getWinner();
     }//end of endGame
 
     public void fillCard(){
-
+        for(int i = 0; i < 4; i++){
+            if(seen1[i] == null){
+                seen1[i] = deck1.pop();
+                return;
+            } else if(seen2[i] == null){
+                seen2[i] = deck1.pop();
+                return;
+            } else if(seen3[i] == null){
+                seen3[i] = deck1.pop();
+                return;
+            }
+        }
     }//end of fillCard
+
+    public Card getCard(int level, int index){
+        Card taken = null;
+        if(index <= 0 || index >= 4)
+            switch(level){
+                case 1: return deck1.pop();
+                case 2: return deck2.pop();
+                case 3: return deck3.pop();
+            }
+        else
+            switch(level){
+                case 1:
+                    taken = seen1[index-1];
+                    seen1[index-1] = null;
+                    break;
+                case 2:
+                    taken = seen2[index-1];
+                    seen2[index-1] = null;
+                    break;
+                case 3:
+                    taken = seen3[index-1];
+                    seen3[index-1] = null;
+                    break;
+            }
+        return taken;
+    }
 
     public Player getWinner(){
         //returns the player that has been determined as the winner
-        if (winner == null)
-            return null;
-        else 
+        if (winner == null){
+            Arrays.sort(players);
+            return players[0];
+        } else 
             return winner;
     }//end of getWinner
 }
