49b790c9adcb7938b103a0855b147a718568bbb1
Day Y <dayanayep982@gmail.com>
2024-11-20 08:05:11 -0600

    worked on endpanel

--- src/EndPanel.java
+++ src/EndPanel.java
@@ -1,11 +1,41 @@
 import java.awt.Graphics;
-public class EndPanel extends ParentPanel{
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseListener;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+
+public class EndPanel extends ParentPanel implements KeyListener{
+    //variables go here vvv
+    private JFrame frame;
+    private Image background = ImageLoader.get("Endscreen_Backgound.png");
+
     public EndPanel(){
+        
         
     }//end of constructor
 
     public void paint(Graphics g){
 
     }//end of paint
+
+    public void keyTyped(KeyEvent e) {
+        //when 'R' is pressed, restart the game
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyPressed'");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'keyReleased'");
+    }
+
+
 
 }//end of class
