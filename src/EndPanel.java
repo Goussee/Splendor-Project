c68a0eabcddd419723718d756511b1c850b77406
Victor-NoC <vitorjonesanicio@gmail.com>
2024-11-22 07:28:09 -0600

    Fixing a small endpanel error, testing

--- src/EndPanel.java
+++ src/EndPanel.java
@@ -1,7 +1,10 @@
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
