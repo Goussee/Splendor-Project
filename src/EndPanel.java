7261d7445db561192c2e28a956ce02c4d0f0a1f7
Day Y <dayanayep982@gmail.com>
2024-11-22 07:55:48 -0600

    yes chaos wow

--- src/EndPanel.java
+++ src/EndPanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.imageio.ImageIO;
@@ -7,15 +9,24 @@ import javax.swing.*;
 public class EndPanel extends ParentPanel {
     //variables go here vvv
     private JFrame frame;
-    private Image background = ImageLoader.get("Endscreen_Backgound.png");
+    private Image background = ImageLoader.get("/Assets/Endscreen_Backgound.png");
+    private Player[] players;
 
     public EndPanel(){
-        
-        
+
+    }
+
+    public EndPanel(Player[] p){
+        players = p;
     }//end of constructor
 
     public void paint(Graphics g){
-
+        g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+        g.setColor(new Color(255,255,255,50));
+        g.fillRect(20, 20, getWidth()-800, getHeight()-50);
+        g.setColor(new Color(255, 60, 79) );
+        g.setFont(new Font("Consolas", 0, 70));
+        g.drawString("Player "+ " is the winner!", 60, 100);
     }//end of paint
 
     
