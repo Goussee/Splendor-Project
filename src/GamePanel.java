dec0101262f740af9df133f8fc7e8a56a2dd0e30
Victor-NoC <vitorjonesanicio@gmail.com>
2024-12-05 07:20:26 -0600

    Adding Player Number Backgrounds

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -23,13 +23,13 @@ public class GamePanel extends ParentPanel implements MouseListener{
 
         switch(numPlayers){
             case 2 : 
-                gameBG = ImageLoader.get("/Assets/GamePanelHelp.png").getScaledInstance(1920, 1080, Image.SCALE_SMOOTH);
+                gameBG = ImageLoader.get("/Assets/Splendor2PlayerBG.png").getScaledInstance(1920, 1080, Image.SCALE_SMOOTH);
                 break;
             case 3: 
-                gameBG = ImageLoader.get("/Assets/GamePanelHelp.png").getScaledInstance(1920, 1080, Image.SCALE_SMOOTH);
+                gameBG = ImageLoader.get("/Assets/Splendor3PlayerBG.png").getScaledInstance(1920, 1080, Image.SCALE_SMOOTH);
                 break;
             case 4: 
-                gameBG = ImageLoader.get("/Assets/GamePanelHelp.png").getScaledInstance(1920, 1080, Image.SCALE_SMOOTH);
+                gameBG = ImageLoader.get("/Assets/Splendor4PlayerBG.png").getScaledInstance(1920, 1080, Image.SCALE_SMOOTH);
                 break;
         }
 
